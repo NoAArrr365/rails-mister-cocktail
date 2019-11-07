6df3f4310a0d422bbce7e9668535d70d12f1f98d
Arnaud Benveniste <arnaud.benveniste@gmail.com>
2019-11-07 16:01:31 +0100

    done

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,23 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    @dose.ingredient = @ingredient
+    @dose.save
+    redirect_to @cocktail
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+end
